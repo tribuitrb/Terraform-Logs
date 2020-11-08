0e97c6878f1fb483aa27af5f6baa0bd5f9c50263
tribuitrb <tribui.trb@gmail.com>
2020-11-09 05:48:22 +0700

    Add Terraform GCP

---
+++ terraform-google/1-tf/README.md
@@ -0,0 +1,1 @@
+# Get project ID: trb-project-295021
---
+++ terraform-google/1-tf/main.tf
@@ -0,0 +1,68 @@
+# Tutorial: https://cloud.google.com/community/tutorials/getting-started-on-gcp-with-terraform
+# get key: https://console.cloud.google.com/apis/credentials/serviceaccountkey
+# Region: https://cloud.google.com/compute/docs/regions-zones
+provider "google" {
+  credentials = file("TrB Project-7851ca716693.json")
+  project     = "trb-project-295021"
+  region      = "asia-southeast1-b"
+}
+
+// Add firewall
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+// Add single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "asia-southeast1-b"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-lts"
+    }
+  }
+
+  // Local SSD disk
+#   scratch_disk {
+#     interface = "SCSI"
+#   }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "trb:${file("id_rsa.pub")}"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+// A variable for extracting the external IP address of the instance
+output "ip" {
+ value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+}
